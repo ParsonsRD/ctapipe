58145d21e9c2a942f5627a5c1573acfd61be1459
Dan Parsons <parsonsrd@gmail.com>
2016-03-09 13:33:06 +0100

    Sanitised inputs so this is not just for mc

--- ctapipe/calib/camera/mc.py
+++ ctapipe/calib/camera/mc.py
@@ -12,54 +12,16 @@ from pyhessio import *
 from ctapipe import io
 from astropy import units as u
 from time import time
+from numba import jit,njit
+import logging
 
 __all__ = [
-    'pixel_integration_mc',
-    'calibrate_amplitude_mc',
+    'pixel_integration',
+    'calibrate_amplitude',
     'calibrate_tpeak'
 ]
 
 CALIB_SCALE = 0.92
-
-def pixel_integration_mc(event, ped, telid, integration_type = "global",window = [7,2]):
-
-    """Integrate the raw adc traces by using one of several algorithms (see functions for
-    algorithm description)
-
-    Parameters
-    ----------
-    geom: `ctapipe.io.CameraGeometry`
-        Camera geometry information
-    ped: list
-        list of pedestals in all gain channels in all pixels
-    pedvars: int
-        Telescope ID number
-    integration_type: string
-        specify signal integration type
-    window: list
-        give dimensions of window to be read out in ADC bins (length, offset from peak)
-
-    Returns
-    -------
-
-    numpy ndarray of integrated signals (in ADC counts) with the dimensions
-    [Gain Channel][pixel number]
-    To Convert this to p.e. this should be passed on to calibrate_pixel_amplitude
-
-    """
-
-    # Firstly copy everything to numpy array to make integrations easier/faster
-    pixel_adc = pixels_to_array(telid, ped )
-    if integration_type == "local":
-        adc_sum = local_peak_integration(pixel_adc,window)
-    elif integration_type == "global":
-        adc_sum = global_peak_integration(pixel_adc,window)
-    elif integration_type == "neighbour":
-        adc_sum = neighbour_peak_integration(event,telid,pixel_adc,window)
-    elif integration_type == "full":
-        adc_sum = full_integration(pixel_adc)
-
-    return adc_sum
 
 def pixels_to_array(telid,ped):
     """
@@ -150,18 +112,17 @@ def global_peak_integration(pixel_adc, window):
 
     return np.sum(pixel_adc,axis=2) #return sum of traces
 
-def neighbour_peak_integration(event,telid,pixel_adc, window, add_central=False):
+def neighbour_peak_integration(pixel_adc,geom,window, add_central=False):
     """
     Integrate signal based on the local peak of all ADC counts of neighbouring pixels.
     Loop currently used which may slow things down (not sure how to get round this)
 
     Parameters
     ----------
-    event: pyhessio event class
-    telid: int
-        telescope number
     pixel_adc: numpy array
         pixel ADC traces with dimensions [gain channel][pixel number][ADC bin]
+    geom:
+        camera geometry class
     window: list
         give dimensions of window to be read out in ADC bins (length, offset from peak)
     add_central: bool
@@ -177,22 +138,90 @@ def neighbour_peak_integration(event,telid,pixel_adc, window, add_central=False)
     #correlate image first using ndimage
     pixel_adc_corr = ndimage.correlate1d(pixel_adc,np.ones(nbins),origin=-1*offset,axis=2,mode="constant")
 
-    pix_x, pix_y = event.meta.pixel_pos[telid] # first get camera geometry (this could be passed in a nicer way)
-    geom = io.CameraGeometry.guess(pix_x, pix_y) # most of time taken here, should think of better may to pass geometry
-
-    signal = np.zeros(pixel_adc.shape[:-1])
-    for gain in range(pixel_adc.shape[0]): # loop over gains and pixels (should be eliminated if possible)
-        for pixel in range(pixel_adc.shape[1]):
-            neighbours = geom.neighbors[pixel] # get list of neighbours
-            if(add_central):
-                neighbours.append(pixel)
-
-            peakbin = np.argmax(np.sum(pixel_adc[gain][neighbours],axis=0)) # sum traces of neighbours and find peak bin
-            signal[gain][pixel] = pixel_adc_corr[gain][pixel][peakbin] #fill signal with correlated values at peak
+    geometry =  geom.neighbors
+    signal = neighbour_loop(pixel_adc,pixel_adc_corr,geometry,add_central)
 
     return signal #return integrated charges
 
-def calibrate_amplitude_mc(integrated_charge, calib, telid,linear_range=[-1000,10000]):
+def neighbour_loop(pixel_adc,pixel_adc_corr,neighbors,add_central):
+    """
+    Loop function for neighbour integration (this code is separated to aid further optimisation).
+
+    Parameters
+    ----------
+    pixel_adc: numpy array
+        pixel ADC traces with dimensions [gain channel][pixel number][ADC bin]
+    pixel_adc_corr: numpy array
+        pixel ADC traces with window correlation applied
+        with dimensions [gain channel][pixel number][ADC bin]
+    neighbours: list
+        list of neighbours for each pixel
+    add_central: bool
+        include the pixel being integrated in the peak determination
+    Returns
+    -------
+    numpy ndarray of integrated signals (in ADC counts) with the dimensions
+    [Gain Channel][pixel number]
+    """
+
+    signal = np.zeros(pixel_adc.shape[:-1])
+
+    for gain in range(pixel_adc.shape[0]): # loop over gains and pixels (should be eliminated if possible)
+        for pixel in range(pixel_adc.shape[1]):
+            nn = neighbors[pixel] # get list of neighbours
+            if(add_central):
+                nn.append(pixel)
+
+            peakbin = np.argmax(np.sum(pixel_adc[gain][nn],axis=0)) # sum traces of neighbours and find peak bin
+            signal[gain][pixel] = pixel_adc_corr[gain][pixel][peakbin] #fill signal with correlated values at peak
+    return signal
+
+
+def pixel_integration(event, ped, telid, integration_type = "global",geometry=None,window = [7,2]):
+
+    """Integrate the raw adc traces by using one of several algorithms (see functions for
+    algorithm description)
+
+    Parameters
+    ----------
+    geom: `ctapipe.io.CameraGeometry`
+        Camera geometry information
+    ped: list
+        list of pedestals in all gain channels in all pixels
+    pedvars: int
+        Telescope ID number
+    integration_type: string
+        specify signal integration type
+    window: list
+        give dimensions of window to be read out in ADC bins (length, offset from peak)
+
+    Returns
+    -------
+
+    numpy ndarray of integrated signals (in ADC counts) with the dimensions
+    [Gain Channel][pixel number]
+    To Convert this to p.e. this should be passed on to calibrate_pixel_amplitude
+
+    """
+    logger = logging.getLogger("integration")
+
+    # Firstly copy everything to numpy array to make integrations easier/faster
+    pixel_adc = pixels_to_array(telid, ped )
+    if integration_type == "local":
+        adc_sum = local_peak_integration(pixel_adc,window)
+    elif integration_type == "global":
+        adc_sum = global_peak_integration(pixel_adc,window)
+    elif integration_type == "neighbour":
+        if geometry == None:
+            raise ValueError("For next neighbour integration camera geometry must be provided")
+
+        adc_sum = neighbour_peak_integration(pixel_adc,geometry,window)
+    elif integration_type == "full":
+        adc_sum = full_integration(pixel_adc)
+
+    return adc_sum
+
+def calibrate_amplitude(integrated_charge,tpeak,calib, telid,linear_range=[-1000,10000]):
     """
     Convert ADC counts integrated by previous functions to photoelectrons. This is done
     by multiplying by ADC to p.e. ratio, then choosing the correct gain channel based
@@ -214,16 +243,18 @@ def calibrate_amplitude_mc(integrated_charge, calib, telid,linear_range=[-1000,1
     numpy ndarray of integrated signals (in photoelectrons) with the dimensions
     [pixel number]
     """
+
     linear_min,linear_max = linear_range
     if integrated_charge is None:
         return None
 
     amplitude = np.zeros(integrated_charge.shape[1:])
+    peaks = np.zeros(integrated_charge.shape[1:])
+
     if get_num_channel(telid) == 1: # If only 1 gain, just multiply everything
         amplitude = integrated_charge[0] * calib[0] * CALIB_SCALE
         return amplitude # return 0th channel (as there is only 1)
     else:
-        amplitude_final = np.zeros(integrated_charge.shape[1:])
         valid_last = np.zeros(integrated_charge.shape[1:])
         for i in range(amplitude.shape[0]):# loop over channels
             # first check that all elements in this array lie in the linear range
@@ -233,10 +264,12 @@ def calibrate_amplitude_mc(integrated_charge, calib, telid,linear_range=[-1000,1
             valid = np.logical_and(valid,np.logical_not(valid_last))
             #Fill final amplitude for only pixels in the linear range
             amplitude += integrated_charge[i] * calib[i] * CALIB_SCALE * valid
+            peaks += tpeak[i] * valid
+
             #Add pixels used in this channel to the total
             valid_last += valid
 
-    return amplitude
+    return amplitude,peaks
 
 def get_max_bin(integrated_charge):
     return np.argmax(integrated_charge,axis=2)
