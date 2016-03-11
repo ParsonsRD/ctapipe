3a7a6b1a0a62154e9d1015cdd1d8a2c507dfcb04
Dan Parsons <parsonsrd@gmail.com>
2016-03-11 17:03:37 +0100

    Changed to calculation using numpy arrays

--- ctapipe/reco/hillasintersection.py
+++ ctapipe/reco/hillasintersection.py
@@ -21,56 +21,56 @@ __all__ = [
 ]
 
 
-def intersect_nominal(hillas_parameters):
+def intersect_nominal(hillas_parameters,weighting="Konrad"):
     if len(hillas_parameters)<2:
         return None
 
-    print("here")
-    hillas_pairs = itertools.combinations((hillas_parameters), 2)
-    print (hillas_pairs)
+    hillas_pairs = list(itertools.combinations(hillas_parameters, 2))
+    h1 = list(map(lambda h:[h[0].psi,h[0].cen_x,h[0].cen_y,h[0].size],hillas_pairs))
+    h1 = np.array(h1)
+    h1 = np.transpose(h1)
 
-    sum_x = 0
-    sum_y = 0
-    sum_w = 0
+    h2 = np.array(list(map(lambda h:[h[1].psi,h[1].cen_x,h[1].cen_y,h[1].size],hillas_pairs)))
+    h2 = np.array(h2)
+    h2 = np.transpose(h2)
 
-    for hill in hillas_pairs:
-        h1,h2 = hill
-        #print (h1,h2)
-        x1,y1 = intersect_lines(h1.cen_x,h1.cen_y,h1.psi,h2.cen_x,h2.cen_y,h2.psi)
-        print(h2.psi,h1.psi,h1.cen_x,h2.cen_x)
-        print("pos",x1,y1,h1.size,h2.size)
-        weight = weight_amplitude(h1.size,h2.size)
-        sum_x+=x1 * weight
-        sum_y+=y1 * weight
-        sum_w+=weight
+    sx,sy = intersect_lines(h1[1],h1[2],h1[0],
+                            h2[1],h2[2],h2[0])
+    if weighting == "Konrad":
+        weight_fn = weight_konrad
+    elif weighting == "HESS":
+        weight_fn = weight_HESS
 
-    print ((sum_x/sum_w),(sum_y/sum_w))
+    weight = weight_fn(h1[3],h2[3])
+    weight *= weight_sin(h1[0],h2[0])
+
+    sum_x = np.sum(sx*weight)
+    sum_y = np.sum(sy*weight)
+    sum_w = np.sum(weight)
+
+    print (sum_x/sum_w,sum_y/sum_w)
+    return sum_x/sum_w,sum_y/sum_w
 
 def intersect_lines(xp1,yp1,phi1,xp2,yp2,phi2):
 
-    #/* Hesse normal form for line 1 */
-    s1 = math.sin(phi1)
-    c1 = math.cos(phi1)
+    s1 = np.sin(phi1)
+    c1 = np.cos(phi1)
     A1 = s1
     B1 = -1*c1
     C1 = yp1*c1 - xp1*s1
 
-    #/* Hesse normal form for line 2 */
-    s2 = math.sin(phi2)
-    c2 = math.cos(phi2)
+    s2 = np.sin(phi2)
+    c2 = np.cos(phi2)
     A2 = s2
     B2 = -1*c2
     C2 = yp2*c2 - xp2*s2
-
-   # print("hmm1",A1,B1,C1,yp1,xp1,c1,s1, yp1*c1 ,xp1*s1)
-   # print("hmm2",A2,B2,C2,yp2,xp2,c2,s2, yp2*c2 ,xp2*s2)
 
     detAB = (A1*B2-A2*B1)
     detBC = (B1*C2-B2*C1)
     detCA = (C1*A2-C2*A1)
 
-    if  math.fabs(detAB) < 1e-14 : # /* parallel */
-        return 0,0
+    #if  math.fabs(detAB) < 1e-14 : # /* parallel */
+    #    return 0,0
 
     xs = detBC / detAB
     ys = detCA / detAB
@@ -78,5 +78,11 @@ def intersect_lines(xp1,yp1,phi1,xp2,yp2,phi2):
     return xs,ys
 
 
-def weight_amplitude(amp1,amp2):
-    return (amp1*amp2)/(amp1+amp2)
+def weight_konrad(p1,p2):
+    return (p1*p2)/(p1+p2)
+
+def weight_HESS(p1,p2):
+    return 1/((1/p1)+(1/p2))
+
+def weight_sin(phi1,phi2):
+    return np.sin(np.fabs(phi1-phi2))
